a44f7508aee8ee638f8ad02b188d07e59e5f90c6
luca <beccibrian@gmail.com>
2021-04-09 21:18:26 -0300

    chore(Pagination): Finished

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const paginate = function (items, page, itemsPerPage) {
-    /* YOUR CODE HERE */
-
+    let toModifyArr = [...items]
+    let itemsOnPage = toModifyArr.splice(((itemsPerPage * page) - itemsPerPage), itemsPerPage)
+    return {
+        totalItems: items.length,
+        itemsOnPage,
+        page,
+        itemsPerPage,
+        from: itemsOnPage[0].id,
+        to: itemsOnPage[itemsOnPage.length - 1].id,
+        prev: ((itemsPerPage * page) - itemsPerPage) > 0 ? true : false,
+        next: (((itemsPerPage * page) - itemsPerPage) + itemsPerPage) < items.length ? true : false
+    }
 }
 
 module.exports = paginate
